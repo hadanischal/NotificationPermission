0a07d0b1d9f2570225e8925b0c88222058d24628
Nischal Hada <hadanischal@gmail.com>
2019-12-24 15:22:38 +1100

    Initial project setup

---
+++ NotificationPermission/NotificationPermission.h
@@ -0,0 +1,19 @@
+//
+//  NotificationPermission.h
+//  NotificationPermission
+//
+//  Created by Nischal Hada on 24/12/19.
+//  Copyright © 2019 Nischal Hada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NotificationPermission.
+FOUNDATION_EXPORT double NotificationPermissionVersionNumber;
+
+//! Project version string for NotificationPermission.
+FOUNDATION_EXPORT const unsigned char NotificationPermissionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationPermission/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'NotificationPermission' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  
+  # Pods for NotificationPermission
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+  pod 'SwiftLint'
+  target 'NotificationPermissionTests' do
+    # Pods for testing
+    pod 'RxBlocking', '~> 5.0.0'
+    pod 'RxTest',     '~> 5.0.0'
+    pod 'Cuckoo', '~> 1.0.6'
+    pod 'Quick', '~> 2.1.0'
+    pod 'Nimble', '~> 8'
+  end
+  
+end
