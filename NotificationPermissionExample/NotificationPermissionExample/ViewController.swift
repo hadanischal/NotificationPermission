3d6915dad7e4ff3855013b8d0332b992f5208e61
Nischal Hada <hadanischal@gmail.com>
2020-01-11 23:28:14 +1100

    Implemented NotificationPermissionExample

---
+++ NotificationPermissionExample/NotificationPermissionExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  NotificationPermissionExample
+//
+//  Created by Nischal Hada on 4/1/20.
+//  Copyright © 2020 Nischal Hada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
